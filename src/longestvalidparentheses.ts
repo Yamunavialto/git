5bb82fd0c3a9a34e6feb4bdb542a0b62d1f2d768
yamuna <pasupuleti.gangayamuna@gmail.com>
2025-06-06 19:09:52 +0530

    Multiply strings

---
+++ src/longestvalidparentheses.ts
@@ -0,0 +1,21 @@
+function longestvalidparentheses(s:string):number{
+    const stack:number[]=[-1];
+    let maxLen=0;
+    for(let i =0;i<s.length;i++){
+        if(s[i]==='('){
+            stack.push(i);
+            }else{
+                stack.pop();
+                if(stack.length===0){
+                    stack.push(i);
+                }else{
+                    maxLen =Math.max(maxLen,i-stack[stack.length-1]);
+                }
+            }
+        }
+    return maxLen;
+}
+
+console.log(longestvalidparentheses("(()"));
+console.log(longestvalidparentheses(")()())"));
+console.log(longestvalidparentheses(""));
