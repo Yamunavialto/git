d8e4e95cf94673bf11e9d50644b124def9508498
yamuna <pasupuleti.gangayamuna@gmail.com>
2025-06-13 19:11:59 +0530

    Simplify path

---
+++ src/simplifypath.ts
@@ -0,0 +1,28 @@
+function simplifyPath(path: string): string {
+    const stack: string[] = [];
+    const parts = path.split('/');
+
+    for (const part of parts) {
+        if (part === '' || part === '.') {
+           
+            continue;
+        } else if (part === '..') {
+           
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            
+            stack.push(part);
+        }
+    }
+
+    return '/' + stack.join('/');
+}
+
+
+console.log(simplifyPath("/home/"));                       
+console.log(simplifyPath("/home//foo/"));                  
+console.log(simplifyPath("/home/user/Documents/../Pictures")); 
+console.log(simplifyPath("/../"));                         
+console.log(simplifyPath("/.../a/../b/c/../d/./"));      
